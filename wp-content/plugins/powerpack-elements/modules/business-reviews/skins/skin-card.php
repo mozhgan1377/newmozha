8146b7e95dcd04e32e8114fa9a1ab6f8aef2aeb9
mozhgan1377 <145985567+mozhgan1377@users.noreply.github.com>
2023-10-23 11:40:52 +0330

    change project

---
+++ wp-content/plugins/powerpack-elements/modules/business-reviews/skins/skin-card.php
@@ -0,0 +1,57 @@
+<?php
+namespace PowerpackElements\Modules\BusinessReviews\Skins;
+
+use PowerpackElements\Base\Powerpack_Widget;
+use PowerpackElements\Modules\Posts\Module;
+
+// Elementor Classes
+use Elementor\Controls_Manager;
+use Elementor\Utils;
+use Elementor\Group_Control_Image_Size;
+use Elementor\Group_Control_Background;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Typography;
+use Elementor\Core\Schemes\Typography as Scheme_Typography;
+use Elementor\Core\Schemes\Color as Scheme_Color;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Card Skin for Business Reviews widget
+ */
+class Skin_Card extends Skin_Base {
+
+	/**
+	 * Retrieve Skin ID.
+	 *
+	 * @access public
+	 *
+	 * @return string Skin ID.
+	 */
+	public function get_id() {
+		return 'card';
+	}
+
+	/**
+	 * Retrieve Skin title.
+	 *
+	 * @access public
+	 *
+	 * @return string Skin title.
+	 */
+	public function get_title() {
+		return __( 'Card', 'powerpack' );
+	}
+
+	/**
+	 * Register Control Actions.
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls_actions() {
+
+		parent::_register_controls_actions();
+	}
+}
